9fd3a49453d8a280bf815a483aafe8adf4d65811
SashHey <60923007+SashHey@users.noreply.github.com>
2024-08-28 11:03:18 +0200

    Added challenge e

---
+++ Challenge e/alex.js
@@ -0,0 +1,6 @@
+const firstname = "Alex"
+const surname = "Naidoo"
+export const role = "Head of Marketing"
+
+const display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#alex').innerText = display
