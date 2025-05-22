caa9df4332828ec2590251d613a52ec85a8473e9
tawandakasunzuma <tskasunzuma@gmail.com>
2025-05-22 10:06:04 +0200

    correct syntax errors in Challenge 3 js file

--- Challenge 3/Challenge f/script.js
+++ Challenge 3/Challenge f/script.js
@@ -1,33 +1,33 @@
-const date: 2050
-const status: 'student'
-const count = 0
+const date = 2050;
+const status = 'student';
+let count = 0;
 
-if (date = 2050) {
-	console.log("January", 'New Year’s Day'
+if (date === 2050) {
+	console.log("January", 'New Year’s Day')
 	console.log("March", 'Human Rights Day')
-	date = 'April'
-	console.log(date, 'Family Day')
-	console.log(date, 'Freedom Day')
-	let count = count + 4
+	let month = 'April'
+	console.log(month, 'Family Day')
+	console.log(month, 'Freedom Day')
+	count = count + 4;
 
-	if (status = "student") {
+	if (status === 'student') {
 	  console.log('June', 'Youth Day')
-		let count = count + 1
-  }
+		count = count + 1
+  	}
 
 	console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
-	date = 'December'
-	console.log(date, 'Day of Reconciliation')
-	let count = count + 3
+	month = 'December'
+	console.log(month, 'Day of Reconciliation')
+	count = count + 3
 
-	if (status = "parent") {
-	  console.log(date, 'Christmas Day')
-		let count = count + 1
+	if (status === "parent") {
+	  console.log(month, 'Christmas Day')
+		count = count + 1
   }
 
-	console.log(date, 'Day of Goodwill')
-	let count = count + 1
+	console.log(month, 'Day of Goodwill')
+	count = count + 1
 }
 
 console.log('Your status is:', status)
