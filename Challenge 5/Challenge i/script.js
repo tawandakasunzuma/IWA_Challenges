d45de78a6ac4eb45ca9b781d0b3401f200d5f5b7
tawandakasunzuma <tskasunzuma@gmail.com>
2025-07-10 10:14:27 +0200

    make corrections in Challenge i folder in scripts.js file

--- Challenge 5/Challenge i/script.js
+++ Challenge 5/Challenge i/script.js
@@ -3,15 +3,19 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
+const hourOfDay = 0;
+const minuteOfDay = 0;
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
+const isTime = hourOfDay === 0 && minuteOfDay === 0;
+let balance = undefined;
+
+if (isTime) {
+  const taxAsDecimal = parseInt(tax) / 100;
+  const startingAfterTax = salary * (1 - taxAsDecimal);
+  const expenses = rent + food + transport;
+balance = startingAfterTax - expenses
 }
 	
-console.log(balance.toFixed(3))
+console.log("R" + balance.toFixed(2))
